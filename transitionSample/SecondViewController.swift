f0c528bf57d58608bbe5d3f11b70f2447ddb0543
MizHiro <pencil3274@gmail.com>
2021-07-13 00:08:23 +0900

    implementation of passing value

--- transitionSample/SecondViewController.swift
+++ transitionSample/SecondViewController.swift
@@ -11,7 +11,14 @@ import UIKit
 class SecondViewController: UIViewController {
     // 入力された文字列を表示するラベル
     @IBOutlet weak var inputtedTextLabel: UILabel!
+    
     // 入力された文字列を格納しておくプロパティ
     // ? : nilの場合を含めて定義（UITextFieldの初期値：nil）
     var inputtedText: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // inputtedTextLabelの表示をinpputtedTextの値にする
+        self.inputtedTextLabel.text = self.inputtedText
+    }
 }
--- transitionSample/ViewController.swift
+++ transitionSample/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     // テキストフィールド
     @IBOutlet weak var textField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        textField.placeholder = "文字を入力"
+    }
+    
     // 画面遷移ボタンタップ処理
     @IBAction func didTapTransitionButton(_ sender: Any) {
         // テキストフィールドの値を代入
@@ -23,24 +29,23 @@ class ViewController: UIViewController {
         self.performSegue(withIdentifier: "SecondViewController", sender: text)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
     // 画面遷移イベントをフックする（遷移前の事前準備）
     // override : 継承元のクラスにもとから定義されているメソッドを上書きする
     // sender : performSegueのsenderと同値
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // segueからSecondViewControllerを取得
+        // segueからSecondViewControllerを取得（destination：UIViewController型をSecondViewController型にキャスト）
         if let secondViewController = segue.destination as? SecondViewController {
-            // senderをStringにキャストする
+            // sender（Any型）をString型にキャストする
             let paramater = sender as? String
             // SecondViewControllerのinputtedTextにparamaterを設定
             secondViewController.inputtedText = paramater
         }
     }
 
+    // 画面タップでテキストフィールドを閉じる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 }
 
